76c55ce45fe3ff78c18bc55cee41c55073b76809
unknown <T145@protonmail.com>
2022-01-12 13:14:43 -0800

    feat(new): Upgraded the package manager to PNPM

--- server/prepare.bash
+++ server/prepare.bash
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
-npm i nodemon -g
-npm i opencollective-postinstall --save
+npm i -g pnpm
+pnpm i -g nodemon
+pnpm i opencollective-postinstall
 #npm i --no-bin-links --no-optional
