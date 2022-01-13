0ea683ff090e3890158a989f40467e8063e6fe5f
unknown <T145@protonmail.com>
2022-01-13 13:04:03 -0800

    feat(nav): Added a welcome banner

--- application/src/components/common/template.js
+++ application/src/components/common/template.js
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { Nav } from '../../components';
 import './template.css';
 
 const Template = props => {
+    const auth = useSelector((state) => state.auth);
+
     return (
         <div className="bg-layer">
             <div className="fg-layer">
                 <label className="logo">Bruce's Diner</label>
+                <label className="welcome">Welcome, {auth.email || 'Unknown'}!</label>
                 <Nav />
                 {props.children}
             </div>
