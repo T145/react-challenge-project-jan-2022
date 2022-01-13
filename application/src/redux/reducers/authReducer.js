92e858be24adc00a9fa06577b01b0668ec092ac0
unknown <T145@protonmail.com>
2022-01-12 21:50:19 -0800

    fix(login): Removed debug statement from login logic

--- application/src/redux/reducers/authReducer.js
+++ application/src/redux/reducers/authReducer.js
@@ -3,7 +3,6 @@ import { LOGIN, LOGOUT } from '../actions/types'
 const INITIAL_STATE = { email: '', token: null };
 
 export default (state = INITIAL_STATE, action) => {
-    console.log(action)
     switch (action.type) {
         case LOGIN:
             return { ...state, email: action.payload.email, token: action.payload.token }
